6b523b9725f1172d2369d9e1389f91091a0560d4
qiuchengjia <qiuqiuchengjia@163.com>
2016-09-01 18:44:04 +0800

    修改了文件名，使得适应客户端

--- connection.php
+++ connection.php
@@ -3,10 +3,10 @@
  * 这个是链接数据库的代码
 */
 //创建连接
-$conn = mysql_connect("localhost","qiu","qiuchengjia")or die("fhdofhd");
+$conn = mysql_connect("localhost","root","")or die("链接失败");
 //设置字符集
 mysql_query("set names 'utf8'");
 //链接数据库
-mysql_select_db("course",$conn)or die("fdf");
+mysql_select_db("qiu",$conn)or die("数据库连接失败");
 
 ?>
